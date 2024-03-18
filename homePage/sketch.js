427337c8e9dd5ddac763521e4f36afa3150ed43c
msabeeh2005 <msabeeh@asu.edu>
2024-03-18 16:39:57 -0700

    3/18 Final

--- homePage/sketch.js
+++ homePage/sketch.js
@@ -1,10 +1,21 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 400);
+  
 }
 
 function draw() {
   background(181,215,168);
   
+  fill('white');
   circle(30, 30, 20);
   circle(30, 55, 30);
+  
+    fill('black');
+   textSize(16);
+   text('Welcome again XYZ ………. Get started by playing any of the games below!', 100, 50);
+  
+  fill('deeppink');
+  textSize(26);
+  text('Featured Games', 80, 125);
+  text('Recent Games', 80, 250);
 }
