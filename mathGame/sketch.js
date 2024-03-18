c2b312d1b12edce9535c823db460cfdc1357373c
bjgalla2 <45400883+bjgalla2@users.noreply.github.com>
2024-03-18 16:20:05 -0700

    Added  basic layout for math game

---
+++ mathGame/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(181,215,);
+  circle(200, 100, 100);
+  rect(30, 250, 55, 55, 20);
+  rect(170, 250, 55, 55, 20);
+  rect(300,250, 55, 55, 20);
+}
