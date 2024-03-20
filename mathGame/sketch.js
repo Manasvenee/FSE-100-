1bc218494ec28f894fcde2b5ff6193a0d1bd34f7
bjgalla2 <45400883+bjgalla2@users.noreply.github.com>
2024-03-20 15:54:59 -0700

    just committing chanegs

--- mathGame/sketch.js
+++ mathGame/sketch.js
@@ -1,11 +1,30 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 400);
 }
 
 function draw() {
-  background(181,215,);
-  circle(200, 100, 100);
-  rect(30, 250, 55, 55, 20);
-  rect(170, 250, 55, 55, 20);
-  rect(300,250, 55, 55, 20);
+  background(181,215,168);
+  circle(310, 100, 100);
+  rect(40, 250, 70, 100, 20);
+  rect(270, 250, 70, 70, 20);
+  rect(500,250, 70, 70, 20);
+
+  
+  textSize(24);
+  textAlign(CENTER, CENTER); 
+  text("5", 310, 100);
+
+
+  textSize(24);
+  textAlign(CENTER, CENTER); 
+  text("1", 75, 285);
+
+  textSize(24);
+  textAlign(CENTER, CENTER); 
+  text("2", 215, 285);
+
+  textSize(24);
+  textAlign(CENTER, CENTER); 
+  text("3", 345, 285);
+
 }
